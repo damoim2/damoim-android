75981b006d532daf1bb47cadd5f78974f0bbf480
Changhyung <pck4949@naver.com>
2023-02-21 04:27:08 +0900

    Add: 네트워크 통신 클라이언트 추가

---
+++ app/src/main/java/com/damoim/android/data/RetrofitClient.kt
@@ -0,0 +1,36 @@
+package com.damoim.android.data
+
+
+import com.damoim.android.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+object RetrofitClient {
+    private const val BASE_URL = ""
+
+    private val logger =
+        HttpLoggingInterceptor().apply {
+            level =
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+        }
+
+    private val client = OkHttpClient.Builder()
+        .readTimeout(5000, TimeUnit.MILLISECONDS)
+        .connectTimeout(5000, TimeUnit.MILLISECONDS)
+        .addInterceptor(logger)
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
